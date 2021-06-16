b7e1bb684492f9d87f8721ffebd479dc1958f323
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-06-17 01:57:42 +0200

    Update ALI.py

--- ALI.py
+++ ALI.py
@@ -271,12 +271,49 @@ class Ui_MainWindow(QWidget):
     def new_img_name(self):
         
         pass
+    
+    def enhance_image(self,path, new_path="", write=0):
 
+        image = cv2.imread(path)
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+        hist_size = len(hist)
+
+        # Calculate cumulative distribution from the histogram
+        accumulator = []
+        accumulator.append(float(hist[0]))
+        for index in range(1, hist_size):
+            accumulator.append(accumulator[index - 1] + float(hist[index]))
+
+        # Locate points to clip
+        maximum = accumulator[-1]
+        clip_hist_percent = (maximum / 200.0)
+        
+        # Locate left cut
+        minimum_gray = 0
+        while accumulator[minimum_gray] < clip_hist_percent:
+            minimum_gray += 1
+
+        # Locate right cut
+        maximum_gray = hist_size - 1
+        while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+            maximum_gray -= 1
+
+        # Calculate alpha and beta values
+        alpha = 255 / (maximum_gray - minimum_gray)
+        beta = -minimum_gray * alpha
+
+
+        enhanced_image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+        if write == 1:
+            cv2.imwrite(new_path, enhanced_image)
+        return enhanced_image
     
 
-  
-
-if __name__ == "__main__":
+    
+    if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
